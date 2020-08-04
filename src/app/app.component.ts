bec6dcf90bfc0493fcf1028b36d333c998aae1da
Maveto <maveto13@gmail.com>
2020-08-04 16:29:52 -0400

    intro excercises

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'curso-angular';
+
+  ngOnInit(){
+    const aux = [1,2,3,4,5,6,7,8]
+
+    
+  }
 }
