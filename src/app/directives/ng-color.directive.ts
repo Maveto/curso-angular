7a255892764e116c14c519a4ed8b71a8cdba1627
Maveto <maveto13@gmail.com>
2020-08-17 16:23:10 -0400

    Directive Input

--- src/app/directives/ng-color.directive.ts
+++ src/app/directives/ng-color.directive.ts
@@ -1,16 +1,21 @@
-import { Directive, ElementRef, HostListener } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: '[appNgColor]'
 })
 export class NgColorDirective {
 
+  @Input('appNgColor') color;
+  @Input() secondColor
+
   @HostListener('mouseenter') mouseEnter(){
-    this.changeColor("#18E406");
+    this.changeColor(this.color);
+    //this.changeColor("#18E406");
   }
 
   @HostListener('mouseleave') mouseLeave(){
-    this.changeColor("#08B4B5");
+    this.changeColor(this.secondColor);
+    //this.changeColor("#08B4B5");
   }
 
   constructor(private el: ElementRef) {
