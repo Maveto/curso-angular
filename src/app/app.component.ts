3decafa0f64b18229f762e0d39b3d01336fcff1f
Maveto <maveto13@gmail.com>
2020-08-18 16:22:30 -0400

    Output in directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,6 +22,8 @@ export class AppComponent implements OnInit{
 
   numbers = [1,2,3,4,5,6,7,8,9,10];
 
+  directiveAux = 'holaaaaa'
+
   persons1 = [
     {
       name:'Juan',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ExpoPipe } from './pipes/expo.pipe';
 import { PurePipe } from './pipes/pure.pipe';
 import { ImpurePipe } from './pipes/impure.pipe';
 import { NgColorDirective } from './directives/ng-color.directive';
+import { NgFocusDirective } from './directives/ng-focus.directive';
 
 
 
@@ -26,6 +27,7 @@ import { NgColorDirective } from './directives/ng-color.directive';
     PurePipe,
     ImpurePipe,
     NgColorDirective,
+    NgFocusDirective,
   ],
   imports: [
     BrowserModule,
