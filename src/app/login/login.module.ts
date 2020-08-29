f8c250c95c97088aba57c60c750d059be832a611
Maveto13 <maveto13@gamil.com>
2020-08-29 15:00:51 -0400

    own structural directive

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -4,10 +4,14 @@ import { CommonModule } from '@angular/common';
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login/login.component';
 import {MatButtonModule} from '@angular/material/button';
+import {NgElseDirective} from '../directives/ng-else.directive';
 
 
 @NgModule({
-  declarations: [LoginComponent],
+  declarations: [
+    LoginComponent,
+    NgElseDirective
+  ],
   imports: [
     CommonModule,
     LoginRoutingModule,
