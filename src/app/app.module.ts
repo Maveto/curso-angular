b4c5582331cebb4bdeb8eead50b25839942a236d
Maveto13 <maveto13@gamil.com>
2020-08-29 12:16:34 -0400

    angular material installation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {HomeModule} from './modules/home/home.module';
 import {AdminModule} from './modules/admin/admin.module';
 import {ListModule} from './modules/list/list.module';
 import {AppRoutingModule} from './app-routing.module';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
 
 
 @NgModule({
@@ -15,8 +18,10 @@ import {AppRoutingModule} from './app-routing.module';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
     AppRoutingModule,
+    NgbModule,
     HomeModule,
     AdminModule,
     ListModule
