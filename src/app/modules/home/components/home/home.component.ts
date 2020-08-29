d304f2d3d772035c04b27a3a3da860b1ca7332c2
Maveto13 <maveto13@gamil.com>
2020-08-29 12:40:46 -0400

    design with card of angular material

--- src/app/modules/home/components/home/home.component.ts
+++ src/app/modules/home/components/home/home.component.ts
@@ -7,6 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  products = [
+    {
+      description: 'aaaa',
+      imageURL: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+      ownerId: 1,
+      price: 12,
+      title: 'salteña'
+    },
+    {
+      description: 'aaaa',
+      imageURL: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+      ownerId: 1,
+      price: 12,
+      title: 'salteña'
+    },
+    {
+      description: 'aaaa',
+      imageURL: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+      ownerId: 1,
+      price: 12,
+      title: 'salteña'
+    },
+    {
+      description: 'aaaa',
+      imageURL: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+      ownerId: 1,
+      price: 12,
+      title: 'salteña'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
