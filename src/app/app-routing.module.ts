453cd38e4f5f9364fedf13278ea1641e00e0e1fe
Maveto13 <maveto13@gamil.com>
2020-08-29 13:45:37 -0400

    architecture login and pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [
+/*const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'home', loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule)},
   {path: 'admin', loadChildren: () => import('./modules/admin/admin.module').then(m => m.AdminModule)},
   {path: 'list', loadChildren: () => import('./modules/list/list.module').then(m => m.ListModule)}
+];*/
+
+const routes: Routes = [
+  {path: '', redirectTo: 'login', pathMatch: 'full'},
+  {path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule)},
+  {path: 'pages', loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule)}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,6 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import {HomeModule} from './modules/home/home.module';
-import {AdminModule} from './modules/admin/admin.module';
-import {ListModule} from './modules/list/list.module';
 import {AppRoutingModule} from './app-routing.module';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
@@ -21,10 +18,7 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
     BrowserAnimationsModule,
     FormsModule,
     AppRoutingModule,
-    NgbModule,
-    HomeModule,
-    AdminModule,
-    ListModule
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
